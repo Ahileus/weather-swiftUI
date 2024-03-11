0a00a03b0774bed2b125ca80b3647d911d7de130
Ivan Rudik <ivan.rudik@iosxpert.biz>
2024-03-11 15:18:23 +0100

    init

---
+++ Weather/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Ivan Rudik on 27.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all
+                )
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
